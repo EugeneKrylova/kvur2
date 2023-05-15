6eb694c7adb9aa92b09f839e03187138fb2be076
Eugene <133597596+EugeneKrylova@users.noreply.github.com>
2023-05-15 20:45:29 +0400

    импорт sqrt

--- kvtest.py
+++ kvtest.py
@@ -1,23 +1,23 @@
-import math
+from math import sqrt
 baddata = True
 while baddata == True:
-try:
-    a = int(input('Введите a: '))
-    b = int(input('Введите b: '))
-    c = int(input('Введите c: '))
-    baddata = False
-except:
-    print('Не удалось получить данные')
+    try:
+        a = int(input('Введите a: '))
+        b = int(input('Введите b: '))
+        c = int(input('Введите c: '))
+        baddata = False
+    except:
+        print('Не удалось получить данные')
 
 D = (b*b)-(4*a*c)
 
-if D>0:
+if D > 0:
 
-    d = math.sqrt(D)
+    d = sqrt(D)
     Х1 =((-b)+d)/(2*a)
     Х2 =((-b)-d)/(2*a)
     print(f'Уравнение имеет два корня X1 ={X1}, X2 ={Х2}')
-elif D==0
+elif D == 0:
     Х1 =(-b)/(2*a)
     print(f'Уравнение имеет один корень X1 ={X1}')
 else:
